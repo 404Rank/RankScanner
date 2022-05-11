e06758d6a354021fa0df64ff501ddf7eebfa8988
404Rank <60612174+404Rank@users.noreply.github.com>
2022-05-11 12:06:50 +0800

    Routine submition
    Routine submition

--- scanner.py
+++ scanner.py
@@ -5,7 +5,9 @@
 import sys
 from lib.usage import usage
 if __name__ == "__main__":
+    print(sys.argv);
     if len(sys.argv) > 1:
         argvs = sys.argv[1:];
+        
         usage.useArg(argvs);
     
