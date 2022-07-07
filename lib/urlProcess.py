23c0e0c60c5103102af52a311afd30f8d340b943
404Rank <60612174+404Rank@users.noreply.github.com>
2022-07-07 21:15:47 +0800

    update
    update

--- lib/urlProcess.py
+++ lib/urlProcess.py
@@ -47,13 +47,13 @@ class urlObject:
         deepList = list(item for item in self.path.split("/") if item !="");
         return deepList;
 
-    def isURL(self,url:str) -> bool:
-        reg = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://  ftp or ftps
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-            r'localhost|' #localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-            r'(?::\d+)?' # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE
-        );
-        return (re.match(reg,url) is not None)
+def isURL(url:str) -> bool:
+    reg = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://  ftp or ftps
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE
+    );
+    return (re.match(reg,url) is not None)
