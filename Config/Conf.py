7da40eaa73d47d91e4304db4f13047600e731421
404Rank <60612174+404Rank@users.noreply.github.com>
2022-07-11 19:25:58 +0800

    Update Conf.py

--- Config/Conf.py
+++ Config/Conf.py
@@ -1,7 +1,19 @@
 #Desc: config
 class config:
+    '''
+    program's version
+    '''
     VERSION = "1.0.0";
+    '''
+    the waitting time for url's reponse
+    '''
     CONN_WAIT_TIME = 3.5;
+    '''
+    show the detials while program running 
+
+    True / False;
+
+    '''
     SHOW_DETIALS = True;
     def __init__(self) -> None:
         pass
